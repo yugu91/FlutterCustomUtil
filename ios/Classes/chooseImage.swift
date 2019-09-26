8f4b47fe42c237a5338abd26756e3747571af59a
zhongyi <yugu91@126.com>
2019-09-26 16:13:11 +0800

    增加 openUrl 插件方法 增加 图片选择 方法

---
+++ ios/Classes/chooseImage.swift
@@ -0,0 +1,63 @@
+//
+//  chooseImage.swift
+//  custom_util_plugin
+//
+//  Created by 钟毅 on 2019/9/26.
+//
+import Flutter
+import Foundation
+import AVFoundation
+class chooseImage: NSObject,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    private var _viewController:UIViewController!;
+    private var _result:FlutterResult!;
+    init(viewController:UIViewController,result: @escaping FlutterResult) {
+        super.init();
+        self._viewController = viewController;
+        self._result = result;
+    }
+    
+    func checkImageAccess(type:UIImagePickerControllerSourceType){
+        if(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)){
+            self.pickImage(type: type);
+        }else{
+            AVCaptureDevice.requestAccess(for: .metadata) { (granted) in
+                if granted {
+                    self.pickImage(type: type);
+                }
+            }
+        }
+    }
+    
+    func pickImage(type:UIImagePickerControllerSourceType) {
+        let imagePicker: UIImagePickerController = UIImagePickerController()
+        imagePicker.modalPresentationStyle = .overCurrentContext
+        imagePicker.delegate = self
+        //照片是否可以编辑
+        imagePicker.allowsEditing = true
+        //imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary 从相册选择照片
+        imagePicker.sourceType = type //拍照
+        //前置照相头是否可用，可用即使用前置摄像头，否则使用后置摄像头
+        //        if UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.front) {
+        //            imagePicker.cameraDevice = UIImagePickerControllerCameraDevice.front;
+        //        } else {
+        //            imagePicker.cameraDevice = UIImagePickerControllerCameraDevice.rear;
+        //        }
+        //跳转到拍照界面或相册
+        _viewController.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            _viewController.dismiss(animated: true) {
+                if #available(iOS 11.0, *) {
+                    if let url = info[UIImagePickerControllerImageURL] as? URL {
+                        self._result(url.absoluteString);
+                    }
+                } else {
+                if let url = info[UIImagePickerControllerReferenceURL] as? URL {
+                    self._result(url.absoluteString);
+                }
+                    // Fallback on earlier versions
+                }
+            }
+    }
+}
