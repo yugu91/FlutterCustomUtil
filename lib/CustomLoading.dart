256f555b9392d99ab71ecec654d85ca9a88ef727
zhongyi <yugu91@126.com>
2019-10-10 21:07:12 +0800

    增加是否使用child作背景

--- lib/CustomLoading.dart
+++ lib/CustomLoading.dart
@@ -6,32 +6,39 @@ class CustomLoading extends StatelessWidget{
   final Widget child;
   final Widget loading;
   final Future future;
+  final bool isViewChild;
   CustomLoading({
-   @required this.child,
+    @required this.child,
     @required this.future,
-   this.loading
+    this.loading,
+    this.isViewChild = true
   });
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Stack(
-      children: <Widget>[
-        FutureBuilder(
-          future: future,
-          builder: (context,state){
-            if(state.connectionState == ConnectionState.waiting){
-              return Positioned(
-                left: 0,top: 0,bottom: 0,right: 0,
-                child: this.loading != null ? this.loading : Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            };
+    var list = <Widget>[
+      FutureBuilder(
+        future: future,
+        builder: (context,state){
+          if(state.connectionState == ConnectionState.waiting){
+            return Positioned(
+              left: 0,top: 0,bottom: 0,right: 0,
+              child: this.loading != null ? this.loading : Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          };
+          if(isViewChild)
             return SizedBox();
-          },
-        ),
-        child
-      ],
+          else
+            return child;
+        },
+      ),
+    ];
+    if(isViewChild)
+      list.add(child);
+    return Stack(
+      children: list,
     );
   }
 
