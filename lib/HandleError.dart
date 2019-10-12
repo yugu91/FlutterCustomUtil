36db8dd4da1bc46646732b00feddd31430ba1c8a
zhongyi <yugu91@126.com>
2019-10-12 22:30:08 +0800

    改用Error

--- lib/HandleError.dart
+++ lib/HandleError.dart
@@ -6,12 +6,15 @@ import 'CustomDialog.dart';
 import 'generated/i18n.dart';
 class CustomError implements Error{
   String msg;
-  bool canReload;
-  String title;
-  CustomError(String msg,{bool canReload = false,String title}){
+  final bool canReload;
+  final String title;
+  final dynamic result;
+  CustomError(String msg,{
+    this.title,
+    this.canReload,
+    this.result
+    }){
      this.msg = msg;
-     this.canReload = canReload;
-     this.title = title;
   }
 
   String toString() {
@@ -24,17 +27,17 @@ class CustomError implements Error{
 }
 
 class HandleError {
-  static Future<int> show(BuildContext context,Exception error,[bool canReload,String title = null]){
+  static Future<int> show(BuildContext context,Error error,[bool canReload,String title = null]){
     var bt = <String>[];
     if(canReload == null){
-      if((error is CustomError && (error as CustomError).canReload) || error is DioError){
+      if((error is CustomError && error.canReload) || error is DioError){
         bt.add(S.of(context).errorTryAgain);
       }
     }else if(canReload == true){
       bt.add(S.of(context).errorTryAgain);
     }
-    if(title == null && error is CustomError && (error as CustomError).title != null)
-      title = (error as CustomError).title;
+    if(title == null && error is CustomError && error.title != null)
+      title = error.title;
     else if (title == null)
       title = S.of(context).errorTitle;
     return CustomDialog.of(context, msg: error.toString(),title: title,bts: bt).show();
